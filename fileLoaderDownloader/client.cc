fb109a8d39818b74de1acc073f7e3536d1f0e506
Roberth Castaño <rcastano@utp.edu.co>
2017-02-13 08:49:03 -0500

    Makefile changed, client.cc and server.cc with zmqpp includes and main

---
+++ fileLoaderDownloader/client.cc
@@ -0,0 +1,9 @@
+#include <zmqpp/zmqpp.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace zmqpp;
+
+int main() {
+  
+}
---
+++ fileLoaderDownloader/server.cc
@@ -0,0 +1,17 @@
+#include <zmqpp/zmqpp.hpp>
+#include <iostream>
+
+// formating the code: clang-format -i server.cc
+
+/*
+It is capable of
+recieving files and save them to disk
+send stored files
+send a list of files stored into disk
+remove files with a command
+*/
+
+
+int main() {
+  
+}
