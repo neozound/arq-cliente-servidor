a8e0c37151e88abe6ff2bbb7e7032114f147b728
Roberth Castaño <rcastano@utp.edu.co>
2017-02-13 10:11:58 -0500

    Buffer tests in client.cc (incomplete)

--- fileTransfer/server.cc
+++ fileTransfer/server.cc
@@ -5,13 +5,25 @@
 
 /*
 It is capable of
+recieving incomming connections of a client
 recieving files and save them to disk
 send stored files
 send a list of files stored into disk
 remove files with a command
 */
 
-
 int main() {
-  
+    cout << "This is the file server\n";
+
+    context ctx;
+    socket s(ctx, socket_type);
+
+    cout << "Binding socket to tcp port 5555\n";
+    s.bind("tcp://*:5555");
+
+    while (true) {
+      message m;
+    }
+    
+    return 0;
 }
