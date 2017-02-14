1dad01d602924f83cf940f4c5cdd2f4fee43a6ed
Roberth Castaño <rcastano@utp.edu.co>
2017-02-14 16:27:55 -0500

    added message pass and list function

--- fileTransfer/client.cc
+++ fileTransfer/client.cc
@@ -17,38 +17,108 @@ remove files with a command
 list the files stored in the server
 */
 
-int main() {
-    streampos size;
-    char * memblock;
 
-    //ate: Is a flag that points the file pointer to the end of the file
-    ifstream file ("escher.jpg", ios::in|ios::binary|ios::ate);
-    ofstream outputFile ("escher2.jpg", ios::binary);
+void listf(socket &s);
+void uploadf(socket &s);
+void downloadf(socket &s);
+void erasef(socket &s);
 
-    if (file.is_open())
+int main(int argc, char const *argv[]) {
+
+    //Created a context (blackbox)
+    context bbox;
+    //Created the socket and the conection
+    socket s(bbox, socket_type::request);
+    s.connect("tcp://localhost:4242");
+
+    char option=' ';
+    do
     {
-      /*
-      tellg() gets the position that points file, in this case it means the
-      total size of file
-      */
-      size = file.tellg();
-      cout << size << endl;
-      // We now request the allocation of a memory as big as size
-      memblock = new char [size];
-      //we position the pointer to the begin of the file
-      file.seekg(0, ios::beg);
-      //we store the content of the read file to memblock
-      file.read(memblock, size);
-      file.close();
+      //display of the options
+      cout << "==== Select an option:" << endl;
+      cout << " * (L)ist" << endl;
+      cout << " * (U)pload" << endl;
+      cout << " * (D)ownload" << endl;
+      cout << " * (E)rase" << endl;
+      cout << " * E(X)it" << endl;
+      cin >> option;
 
-      cout << "the entire file is in memory" << endl;
+      switch(option){
+        case 'L':
+            listf(s);
+            break;
+        case 'U':
+            uploadf(s);
+            break;
+        case 'D':
+            downloadf(s);
+            break;
+        case 'E':
+            erasef(s);
+            break;
+        case 'X':
+            return 0;
+            break;
+        default:
+            cout << "Unrecognized option: " << option << endl;
+        break;
+      }
+    } while (option!='X');
 
-      outputFile.write (memblock, size);
-
-      outputFile.close();
-
-      delete[] memblock;
-    }
-
-    return 0;
+  cout << "Finished." << endl;
+  return 0;
 }
+
+void listf(socket &s){
+    //ask for list
+    message req;
+    req << "list";
+    s.send(req);
+    cout << "List request sent." << endl;
+    //wait for answer
+    message ans;
+    string chain;
+    s.receive(ans);
+    ans >> chain;
+    cout << "Response: " << chain << endl;
+
+}
+
+void uploadf(socket &s){
+  streampos size;
+  char * memblock;
+
+  //ate: Is a flag that points the file pointer to the end of the file
+  ifstream file ("escher.jpg", ios::in|ios::binary|ios::ate);
+  ofstream outputFile ("escher2.jpg", ios::binary);
+
+  if (file.is_open())
+  {
+    /*
+    tellg() gets the position that points file, in this case it means the
+    total size of file
+    */
+    size = file.tellg();
+    cout << size << endl;
+    // We now request the allocation of a memory as big as size
+    memblock = new char [size];
+    //we position the pointer to the begin of the file
+    file.seekg(0, ios::beg);
+    //we store the content of the read file to memblock
+    file.read(memblock, size);
+    file.close();
+
+    cout << "the entire file is in memory" << endl;
+
+    outputFile.write (memblock, size);
+
+    outputFile.close();
+
+    delete[] memblock;
+  }
+
+}
+
+void downloadf(socket &s){}
+
+void erasef(socket &s){}
--- fileTransfer/server.cc
+++ fileTransfer/server.cc
@@ -1,5 +1,9 @@
 #include <zmqpp/zmqpp.hpp>
 #include <iostream>
+#include <string>
+
+using namespace std;
+using namespace zmqpp;
 
 // formating the code: clang-format -i server.cc
 
@@ -12,6 +16,59 @@ send a list of files stored into disk
 remove files with a command
 */
 
-int main() {
-    return 0;
+void listf(socket &s);
+void uploadf(socket &s);
+void downloadf(socket &s);
+void erasef(socket &s);
+
+int main(int argc, char *argv[]) {
+  // initialize the context (blackbox)
+  context bbox;
+  // generate a reply socket
+  socket s(bbox, socket_type::reply);
+  // bind to the socket
+  s.bind("tcp://*:4242");
+
+  while (true) {
+    // receive the message
+    cout << "Receiving message..." << endl;
+    message req;
+    s.receive(req);
+
+    string option;
+    req >> option;
+
+    if (option == "list")
+    {
+      listf(s);
+    }else if (option == "")
+    {
+      /* code */
+    }else if (option == "")
+    {
+      /* code */
+    }else if (option == "")
+    {
+      /* code */
+    }else{
+
+    }
+
+  cout << "Finished." << endl;
 }
+}
+
+
+
+void listf(socket &s){
+    message req;
+    req << "Ok!, List request received";
+    s.send(req);
+    cout << "A client asked for list" << endl;
+}
+
+void uploadf(socket &s){}
+
+void downloadf(socket &s){}
+
+void erasef(socket &s){}
