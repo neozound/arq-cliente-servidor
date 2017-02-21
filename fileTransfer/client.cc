087303412d9f61a54471d29e65926535f9c4a2a6
Oscar Eduardo <oscarbernal93@nictibio>
2017-02-21 13:45:43 -0500

    cambios de Oscar Actualizados

--- fileTransfer/client.cc
+++ fileTransfer/client.cc
@@ -1,46 +1,114 @@
-#include <iostream>
 #include <zmqpp/zmqpp.hpp>
-#include <string>
+#include <iostream>
 #include <fstream>
+#include <iterator>
+#include <vector>
 
 using namespace std;
 using namespace zmqpp;
 
-void clean_message(message& m){
-  while(m.parts() > 0){
-    m.pop_back();
-  }
+/*
+It is capable of
+loading a binary file
+conecting to a server
+recieving files and save them to disk
+send stored files
+remove files with a command
+list the files stored in the server
+*/
+
+
+void listf(socket &s);
+void uploadf(socket &s);
+void downloadf(socket &s);
+void erasef(socket &s);
+
+void clean_message(message& m);
+vector<char> readFileToBytes(const string& fileName) ;
+void fileToMesage(const string& fileName, message& msg);
+void messageToFile(const message& msg, const string& fileName);
+void create_message(const string& cmd, const string& filename, message& msg);
+
+int main(int argc, char const *argv[]) {
+
+    //Created a context (blackbox)
+    context bbox;
+    //Created the socket and the conection
+    socket s(bbox, socket_type::request);
+    s.connect("tcp://localhost:4242");
+
+    char option=' ';
+    cout << "Available options:" << endl;
+    cout << " * (L)ist" << endl;
+    cout << " * (U)pload" << endl;
+    cout << " * (D)ownload" << endl;
+    cout << " * (E)rase" << endl;
+    cout << " * E(X)it" << endl;
+    do
+    {
+      //display of the options
+      cout << "> ";
+      cin >> option;
+
+      switch(option){
+        case 'L':
+            listf(s);
+            break;
+        case 'U':
+            uploadf(s);
+            break;
+        case 'D':
+            downloadf(s);
+            break;
+        case 'E':
+            erasef(s);
+            break;
+        case 'X':
+            return 0;
+            break;
+        default:
+            cout << "Unrecognized option: " << option << endl;
+        break;
+      }
+    } while (option!='X');
+
+  cout << "Finished." << endl;
+  return 0;
 }
 
-vector<char> readFileToBytes(const string& fileName) {
-	ifstream ifs(fileName, ios::binary | ios::ate);
-	ifstream::pos_type pos = ifs.tellg();
+void listf(socket &s){
+    //ask for list
+    message req;
+    req << "list";
+    s.send(req);
+    //wait for answer
+    message ans;
+    string chain;
+    s.receive(ans);
+    ans >> chain;
+    cout << "Files in the server: "<< chain << endl;
 
-	vector<char> result(pos);
-
-	ifs.seekg(0, ios::beg);
-	ifs.read(result.data(), pos);
-
-	return result;
 }
 
-void fileToMesage(const string& fileName, message& msg) {
-	vector<char> bytes = readFileToBytes(fileName);
-	msg.add_raw(bytes.data(), bytes.size());
-}
+void uploadf(socket &s){
+  //declaration
+  streampos size;
+  string filename;
 
-void create_message(const string& cmd, const string& filename, message& msg){
-  msg << cmd << filename;
-}
+  //get the name of the file
+  cout << "write the filename: ";
+  cin >> filename;
+  //check if file exist
+  ifstream infile(filename);
+  if (!infile.good())
+  {
+    cout << "Bad filename!"<< endl;
+    return;
+  }//get out if bad filename
 
-int main(){
+  //the privated client-server comand
   string cmd = "upload";
-  string filename = "lincoln.jpg";
   string strMsg;
-
-  context ctx;
-  socket s(ctx, socket_type::req);
-  s.connect("tcp://localhost:5555");
 
   message m;
   create_message(cmd, filename, m);
@@ -60,5 +128,84 @@ int main(){
   cout << strMsg << endl;
   clean_message(m);
   
-  return 0;
+}
+
+void downloadf(socket &s){
+  //first ask for the file
+  //if file exists wait for answer
+  //if file doesn't exist then exit
+  string filename;
+
+  //get the name of the file
+  cout << "write the filename: ";
+  cin >> filename;
+
+  //the privated client-server comand
+  string cmd = "download";
+  string answer;
+
+  message m;
+  create_message(cmd, filename, m);
+  s.send(m);
+
+  s.receive(m);
+  m >> answer;
+
+  clean_message(m);
+
+  if (answer == "bad")
+  {
+    cout << "Bad filename!"<< endl;
+    return;
+  }else if(answer == "good"){
+    //recive the file
+    s.receive(m);
+    filename = "downloaded_" + filename;
+    messageToFile(m,filename);
+
+    clean_message(m);
+    cout << "Finished" << endl;
+    s.send("ok"); //ACK
+  }
+}
+
+void erasef(socket &s){}
+
+
+//Functions of messaging & file manage
+
+void clean_message(message& m){
+  while(m.parts() > 0){
+    m.pop_back();
+  }
+}
+
+vector<char> readFileToBytes(const string& fileName) {
+  ifstream ifs(fileName, ios::binary | ios::ate);
+  ifstream::pos_type pos = ifs.tellg();
+
+  vector<char> result(pos);
+
+  ifs.seekg(0, ios::beg);
+  ifs.read(result.data(), pos);
+
+  return result;
+}
+
+void fileToMesage(const string& fileName, message& msg) {
+  vector<char> bytes = readFileToBytes(fileName);
+  msg.add_raw(bytes.data(), bytes.size());
+}
+
+void messageToFile(const message& msg, const string& fileName) {
+  const void *data;
+  msg.get(&data, 0);
+  size_t size = msg.size(0);
+
+  ofstream ofs(fileName, ios::binary);
+  ofs.write((char*)data, size);
+}
+
+void create_message(const string& cmd, const string& filename, message& msg){
+  msg << cmd << filename;
 }
--- fileTransfer/server.cc
+++ fileTransfer/server.cc
@@ -1,11 +1,143 @@
-#include <iostream>
 #include <zmqpp/zmqpp.hpp>
+#include <iostream>
 #include <string>
 #include <fstream>
 
 using namespace std;
 using namespace zmqpp;
 
+// formating the code: clang-format -i server.cc
+
+/*
+It is capable of
+recieving incomming connections of a client
+recieving files and save them to disk
+send stored files
+send a list of files stored into disk
+remove files with a command
+*/
+
+void listf(socket &s, const string &files);
+void uploadf(socket &s,string filename, string &files);
+void downloadf(socket &s,string filename);
+void erasef(socket &s, string &files);
+
+void clean_message(message& m);
+void messageToFile(const message& msg, const string& fileName);
+vector<char> readFileToBytes(const string& fileName) ;
+void fileToMesage(const string& fileName, message& msg);
+void create_message(const string& cmd, const string& filename, message& msg);
+
+
+
+int main(int argc, char *argv[]) {
+  // initialize the context (blackbox)
+  context bbox;
+  // generate a reply socket
+  socket s(bbox, socket_type::reply);
+  // bind to the socket
+  s.bind("tcp://*:4242");
+  
+  
+
+  //vector with all the uploaded filenames
+  string files;
+
+  while (true) {
+    // receive the message
+    message req;
+    cout << "Receiving message... ";
+    s.receive(req);
+
+    string option;
+    string fname;
+    req >> option;
+
+    cerr << option << endl;
+
+    if (option == "list")
+    {
+      const string chain(files);
+      listf(s,chain);
+    }else if (option == "upload")
+    {
+      req >> fname;
+      uploadf(s,fname,files);
+    }else if (option == "download")
+    {
+      req >> fname;
+      downloadf(s,fname);
+    }else if (option == "")
+    {
+      /* code */
+    }else{
+
+    }
+
+  cout << "Finished." << endl;
+}
+}
+
+//the name of the function indiques the request of the client
+
+void listf(socket &s, const string &files){
+    message m;
+    m << files;
+    s.send(m);
+    cout << "A client asked for list" << endl;
+}
+
+void uploadf(socket &s,string filename, string &files){
+    message m;
+
+    s.send("Ready to recieve");
+
+    s.receive(m);
+    filename = "uploaded_" + filename;
+    messageToFile(m,filename);
+
+    clean_message(m);
+    cout << "Finished" << endl;
+    s.send("Saved");
+
+    //after saved the file add it to the 
+    files += "\n" + filename;
+}
+
+void downloadf(socket &s,string filename){
+ //declaration
+  streampos size;
+  string strMsg;
+
+  cerr << "here is jhonny";return;
+
+  //check if file exist
+  ifstream infile(filename);
+  if (!infile.good())
+  {
+    s.send("bad");
+    return;
+  }//get out if bad filename
+
+  s.send("good");
+  cout << "Client downloading "<< filename << "... ";
+
+  message m;
+
+  fileToMesage(filename,m);
+  s.send(m);
+
+  s.receive(m);
+
+  m >> strMsg;
+  cout << strMsg << endl;
+  clean_message(m);
+  
+}
+
+void erasef(socket &s, string &files){}
+
+//file manager functions
 void clean_message(message& m){
   while(m.parts() > 0){
     m.pop_back();
@@ -13,38 +145,31 @@ void clean_message(message& m){
 }
 
 void messageToFile(const message& msg, const string& fileName) {
-	const void *data;
-	msg.get(&data, 0);
-	size_t size = msg.size(0);
+  const void *data;
+  msg.get(&data, 0);
+  size_t size = msg.size(0);
 
-	ofstream ofs(fileName, ios::binary);
-	ofs.write((char*)data, size);
+  ofstream ofs(fileName, ios::binary);
+  ofs.write((char*)data, size);
 }
 
-int main(){
-  string cmd;
-  string filename;
-  context ctx;
-  socket s(ctx, socket_type::rep);
-  s.bind("tcp://*:5555");
+vector<char> readFileToBytes(const string& fileName) {
+  ifstream ifs(fileName, ios::binary | ios::ate);
+  ifstream::pos_type pos = ifs.tellg();
 
-  message m;
-  s.receive(m);
-  m >> cmd;
-  m >> filename;
+  vector<char> result(pos);
 
-  clean_message(m);
+  ifs.seekg(0, ios::beg);
+  ifs.read(result.data(), pos);
 
-  cout << cmd << " " << filename << endl;
+  return result;
+}
 
-  s.send("Ready to recieve");
+void fileToMesage(const string& fileName, message& msg) {
+  vector<char> bytes = readFileToBytes(fileName);
+  msg.add_raw(bytes.data(), bytes.size());
+}
 
-  s.receive(m);
-  filename = "down" + filename;
-  messageToFile(m,filename);
-
-  clean_message(m);
-  cout << "Finished" << endl;
-  s.send("Saved");
-  return 0;
+void create_message(const string& cmd, const string& filename, message& msg){
+  msg << cmd << filename;
 }
